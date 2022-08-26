81e863d72f2292f66487ccf7f4e3a8158c9137cc
Michael Irwin <mikesir87@gmail.com>
2022-08-26 15:19:15 -0400

    Add seed SQL and change the default command for python dev stage

--- build-first-website-with-python/.docker/db/01-schema.sql
+++ build-first-website-with-python/.docker/db/01-schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `memes` (
-  `id` int NOT NULL,
+  `id` int NOT NULL PRIMARY KEY AUTO_INCREMENT,
   `url` varchar(255) NOT NULL,
   `creation_date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
