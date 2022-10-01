ed34f2b034f71ae5fa285567e29d62df81d707eb
Michael Irwin <mikesir87@gmail.com>
2022-10-01 15:40:38 -0400

    Include starting index.html, styles.css, and Python function

--- build-first-website-with-python/app/app.py
+++ build-first-website-with-python/app/app.py
@@ -1,7 +1,12 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
-# We will add our custom app functions here to support our website
+# This function will be run whenever requests to "/" are made by the browser.
+# The HTML returned from here will be sent to the browser making the request.
+@app.route("/")
+def index():
+    message = "Hello world!"
+    return render_template("index.html", message=message)
 
 # This starts our Python app
 if __name__ == "__main__":
